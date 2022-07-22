2af95cca9850d61bc26536852632e1e1673b4927
Lucas Gambier <gambier.lucas@gmail.com>
2022-07-22 16:00:58 +0200

    add Episode

---
+++ src/models/Episode.js
@@ -0,0 +1,14 @@
+class Episode {
+    name;
+    date;
+    episode;
+    fav;
+
+    constructor(name, date, episode, fav = false){
+        this.name = name;
+        this.date = date;
+        this.episode = episode;
+        this.fav = fav;
+    }
+
+}
