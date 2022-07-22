f39282e78ce0a1262235738d28a73cb872c54c0b
Sarah Majewski <sarah.majewski80@gmail.com>
2022-07-22 15:42:15 +0200

    Router ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import Home from "./pages/Home";
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+     </BrowserRouter>
     </div>
   );
 }
